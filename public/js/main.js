70ca761f02e1ae4e2aeb9b3a3d9fa63b22233b6d
mohmedvaid <hussainvaid7@gmail.com>
2025-11-24 09:47:52 -0600

    Add initial HTML, CSS, and JavaScript files for pathfinding algorithm visualizer

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+// Main entry point - will be implemented in next steps
+// For now, this is a placeholder to ensure the module loads
+
+console.log('Pathfinding Visualizer loaded');
+
+// Placeholder for Board initialization
+// This will be implemented in the next commit
+
